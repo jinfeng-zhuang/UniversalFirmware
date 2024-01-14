ea4fe49f2a71da9fdde6e3bbdf10b51e7fe80630
Jinfeng Zhuang <jinfeng.zhuang@aliyun.com>
2024-01-14 18:31:56 +0800

    通过配置 ITM 正确的系统时钟，ITM 工作正常。
    Signed-off-by: Jinfeng Zhuang <jinfeng.zhuang@aliyun.com>

--- source/entry/main.c
+++ source/entry/main.c
@@ -4,46 +4,49 @@
 // Jinfeng Zhuang, 2024.Week2
 //_____________________________________________________________________________
 
-
+// C Standard
 #include <stdio.h>
 
+// LL
 #include <stm32f4xx_hal.h>
 
+// HAL
 #include <hal/gpio.h>
 #include <hal/clocktree.h>
 
+// Board
+#include <devicetree/stm32f4_discovery.h>
+
 int fputc(int ch, FILE *f) 
 {
-  //return(ITM_SendChar(ch));
-  return 0;
+  return(ITM_SendChar(ch));
 }
 
 void cpu_init(void)
 {
-#if (INSTRUCTION_CACHE_ENABLE != 0U)
-  __HAL_FLASH_INSTRUCTION_CACHE_ENABLE();
-#endif
-	
-#if (DATA_CACHE_ENABLE != 0U)
-  __HAL_FLASH_DATA_CACHE_ENABLE();
-#endif
-	
-#if (PREFETCH_ENABLE != 0U)
-  __HAL_FLASH_PREFETCH_BUFFER_ENABLE();
-#endif
-	
 	HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
 	
 	HAL_InitTick(TICK_INT_PRIORITY);
 	
 	__HAL_RCC_SYSCFG_CLK_ENABLE();
-  __HAL_RCC_PWR_CLK_ENABLE();
+  	__HAL_RCC_PWR_CLK_ENABLE();
 	
 	clocktree_init(0);
 }
 
 void soc_init(void)
 {
+#if (INSTRUCTION_CACHE_ENABLE != 0U)
+	  __HAL_FLASH_INSTRUCTION_CACHE_ENABLE();
+#endif
+		
+#if (DATA_CACHE_ENABLE != 0U)
+	  __HAL_FLASH_DATA_CACHE_ENABLE();
+#endif
+		
+#if (PREFETCH_ENABLE != 0U)
+	  __HAL_FLASH_PREFETCH_BUFFER_ENABLE();
+#endif
 
 }
 
@@ -71,9 +74,17 @@ int main(void)
   
   task_init();
   
-  hal_gpio_init(PD13);
-  hal_gpio_set(PD13, 1);
-	hal_gpio_set(PD13, 0);
+  hal_gpio_init(LED_USER_ORANGE);
+  hal_gpio_set(LED_USER_ORANGE, 1);
+
+  hal_gpio_init(LED_USER_GREEN);
+  hal_gpio_set(LED_USER_GREEN, 1);
+
+  hal_gpio_init(LED_USER_RED);
+  hal_gpio_set(LED_USER_RED, 1);
+
+  hal_gpio_init(LED_USER_BLUE);
+  hal_gpio_set(LED_USER_BLUE, 1);
 
   printf("Hello ITM\n");
   
